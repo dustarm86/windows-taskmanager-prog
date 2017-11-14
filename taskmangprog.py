9b5ab5c3502c6fb87fb441d9a551512223a96d29
Dustin Armstrong <dieter@Dustins-MacBook-Pro.local>
2017-11-14 14:11:06 -0500

    destiny 2 support

--- taskmangprog.py
+++ taskmangprog.py
@@ -1,10 +1,22 @@
 import subprocess
 
-# runs windows shell command to force teamviewer to close
+# runs windows command prompt command to force teamviewer to close
 subprocess.call('Taskkill /IM TeamViewer.exe /F')
 
-# runs windows shell command to open task manager and change priority for .exe file to high
-subprocess.call('wmic process where name="Overwatch.exe" CALL setpriority 128', shell=True)
+# runs windows compant prompt command to open Destiny 2's .exe shortcut
+subprocess.call('C:\Users\Dustin\Desktop\Destiny2')
+
+# wait 10 secs for battle.net launcher to close
+time.sleep(10)
+
+# re-open destiny 2 shortcut after battle.net has closed
+subprocess.call('C:\Users\Dustin\Desktop\Destiny2')
+
+# wait 5 secs before changing destiny's task manager priority to high
+time.sleep(5)
+
+# runs windows command prompt command to open task manager and change priority for .exe file to high
+subprocess.call('wmic process where name="Destiny2.exe" CALL setpriority 128', shell=True)
 
 """
 Priority Integers:
